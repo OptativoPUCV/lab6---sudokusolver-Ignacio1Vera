81d52f11578dd4376797d2466f0c8eaa16c30373
Replit user <>
2023-10-09 00:05:11 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 #include "list.h"
 
-typedef struct {
-    int sudo[9][9];
-} Node;
+typedef struct{
+   int sudo[9][9];
+}Node;
 
-Node* createNode() {
-    Node* n = (Node*) malloc(sizeof(Node));
-    return n;
+Node* createNode(){
+  Node* n = (Node*) malloc(sizeof(Node));
+  return n;
 }
 
-Node* copy(Node* n) {
+Node* copy(Node* n){
     Node* new = (Node*) malloc(sizeof(Node));
     *new = *n;
     return new;
 }
 
-Node* read_file(char* file_name) {
+Node* read_file(char* file_name){
     Node* n = createNode();
     FILE* file = fopen(file_name, "r");
     int i, j;
@@ -27,12 +27,11 @@ Node* read_file(char* file_name) {
                 printf("failed to read data!");
         }
     }
-
     fclose(file);
     return n;
 }
 
-void print_node(Node* n) {
+void print_node(Node* n){
     int i, j;
     for (i = 0; i < 9; i++) {
         for (j = 0; j < 9; j++)
@@ -42,79 +41,87 @@ void print_node(Node* n) {
     printf("\n");
 }
 
-int is_valid(Node* n) {
-    int rows[9][10] = {0};
-    int cols[9][10] = {0};
-    int subs[9][10] = {0};
+int is_valid(Node* n){
+    int rows[9][10] = {0};  // Para verificar filas
+    int cols[9][10] = {0};  // Para verificar columnas
+    int subgrid[3][3][10] = {0};  // Para verificar subgrids
 
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
+    int i, j;
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
             int num = n->sudo[i][j];
             if (num != 0) {
-                if (rows[i][num] || cols[j][num] || subs[(i / 3) * 3 + j / 3][num]) {
-                    return 0;
-                }
+                // Verificar filas
+                if (rows[i][num] == 1) return 0;
                 rows[i][num] = 1;
+                // Verificar columnas
+                if (cols[j][num] == 1) return 0;
                 cols[j][num] = 1;
-                subs[(i / 3) * 3 + j / 3][num] = 1;
+                // Verificar subgrids
+                if (subgrid[i / 3][j / 3][num] == 1) return 0;
+                subgrid[i / 3][j / 3][num] = 1;
             }
         }
     }
     return 1;
 }
 
-List* get_adj_nodes(Node* n) {
+List* get_adj_nodes(Node* n){
     List* list = createList();
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
+    int i, j;
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
             if (n->sudo[i][j] == 0) {
-                for (int k = 1; k <= 9; k++) {
+                int k;
+                for (k = 1; k <= 9; k++) {
                     Node* newNode = copy(n);
                     newNode->sudo[i][j] = k;
-                    pushBack(list, newNode);
+                    if (is_valid(newNode)) {
+                        pushBack(list, newNode);
+                    } else {
+                        free(newNode);
+                    }
                 }
-                return list;  // Solo necesitas generar nodos adyacentes para la primera casilla vacía encontrada
+                return list;  // Solo necesitamos los nodos adyacentes para la primera celda vacía
             }
         }
     }
-    return list;  // Si no hay casillas vacías, devuelve una lista vacía
+    return list;
 }
 
-int is_final(Node* n) {
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
+int is_final(Node* n){
+    int i, j;
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
             if (n->sudo[i][j] == 0) {
-                return 0;  // Si aún hay una casilla vacía, no es un estado final
+                return 0;  // Todavía hay celdas vacías, no es un estado final
             }
         }
     }
-    return 1;  // Si no hay casillas vacías, es un estado final
+    return 1;  // Todas las celdas están llenas, es un estado final
 }
 
-Node* DFS(Node* initial, int* cont) {
+Node* DFS(Node* initial, int* cont){
     Stack* stack = createStack();
     push(stack, initial);
 
-    while (!isEmpty(stack)) {
-        Node* current = top(stack);
+    while (!is_empty(stack)) {
+        Node* current = (Node*)top(stack);
         pop(stack);
-        (*cont)++;
 
         if (is_final(current)) {
-            return current;  // Encontramos una solución
+            return current;  // Hemos encontrado una solución
         }
 
         List* adj_nodes = get_adj_nodes(current);
-        ListNode* current_node = adj_nodes->first;
-        while (current_node != NULL) {
-            Node* adj_node = (Node*)current_node->data;
-            if (is_valid(adj_node)) {
-                push(stack, adj_node);
-            }
-            current_node = current_node->next;
+        Node* adj_node = (Node*)front(adj_nodes);
+
+        while (adj_node != NULL) {
+            push(stack, adj_node);
+            adj_node = (Node*)next(adj_nodes);
         }
-        freeList(adj_nodes);
-        free(current);
+
+        (*cont)++;
     }
 
     return NULL;  // No se encontró una solución
@@ -122,14 +129,14 @@ Node* DFS(Node* initial, int* cont) {
 
 int main(int argc, char* argv[]) {
     Node* initial = read_file("s12a.txt");
-
     int cont = 0;
     Node* final = DFS(initial, &cont);
     printf("iterations:%d\n", cont);
     if (final) {
+        printf("Solution:\n");
         print_node(final);
     } else {
-        printf("No se encontró una solución.\n");
+        printf("No solution found.\n");
     }
 
     return 0;
